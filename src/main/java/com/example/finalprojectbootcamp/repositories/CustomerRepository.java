0ca14600a44885f9c2831b96360d82a756788a4a
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-26 23:38:44 +0100

    A query for updating the password of both Expert and Customer has been written, which finds and updates the Expert based on their ID.

--- src/main/java/com/example/finalprojectbootcamp/repositories/CustomerRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/CustomerRepository.java
@@ -2,6 +2,15 @@ package com.example.finalprojectbootcamp.repositories;
 
 import com.example.finalprojectbootcamp.core.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CustomerRepository extends JpaRepository<Customer , Long > {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Modifying
+    @Query(
+            """
+                    update Customer c set c.password=:password where c.id=:id
+                    """
+    )
+    int updateCustomerByPassword(long id, String password);
 }
--- src/main/java/com/example/finalprojectbootcamp/repositories/ExpertRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/ExpertRepository.java
@@ -2,6 +2,16 @@ package com.example.finalprojectbootcamp.repositories;
 
 import com.example.finalprojectbootcamp.core.entities.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ExpertRepository extends JpaRepository<Expert , Long> {
+public interface ExpertRepository extends JpaRepository<Expert, Long> {
+    @Modifying
+    @Query(
+            """
+                    update Expert e set e.password =:password where e.id=:id
+                    """
+    )
+    int updateExpertByPassword(long id, String password);
+    Expert findExpertById(long id) ;
 }
