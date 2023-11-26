9bf2e6de13c6002ac6405d6e93ddd7f571a8a1b4
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-26 23:44:31 +0100

    Methods for saving and finding a service by the expert's ID have been implemented in the service layer.And also, the method for finding a service in the sub-service layer was used. If the desired service does not exist when adding a sub-service, it should throw an exception.

--- src/main/java/com/example/finalprojectbootcamp/services/ServiceServiceImpl.java
+++ src/main/java/com/example/finalprojectbootcamp/services/ServiceServiceImpl.java
@@ -1,4 +1,25 @@
 package com.example.finalprojectbootcamp.services;
 
-public class ServiceServiceImpl {
+import com.example.finalprojectbootcamp.core.entities.Service;
+import com.example.finalprojectbootcamp.repositories.ServiceRepository;
+
+import java.util.Optional;
+
+public class ServiceServiceImpl implements ServiceService {
+    private final ServiceRepository serviceRepository ;
+
+    public ServiceServiceImpl(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
+    }
+
+
+    @Override
+    public void addANewService(Service service) {
+        serviceRepository.save(service) ;
+    }
+
+    @Override
+    public Service findServicesById (long id) {
+        return serviceRepository.findServicesById(id);
+    }
 }
--- src/main/java/com/example/finalprojectbootcamp/services/SubServiceServiceImpl.java
+++ src/main/java/com/example/finalprojectbootcamp/services/SubServiceServiceImpl.java
@@ -1,4 +1,31 @@
 package com.example.finalprojectbootcamp.services;
 
-public class SubServiceServiceImpl {
+import com.example.finalprojectbootcamp.core.entities.Service;
+import com.example.finalprojectbootcamp.core.entities.SubService;
+import com.example.finalprojectbootcamp.repositories.SubServiceRepository;
+import com.example.finalprojectbootcamp.util.myExceptions.MyExceptions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class SubServiceServiceImpl implements SubServiceService {
+    private final SubServiceRepository subServiceRepository;
+    private ServiceService serviceService;
+
+    public SubServiceServiceImpl(SubServiceRepository subServiceRepository) {
+        this.subServiceRepository = subServiceRepository;
+    }
+
+    @Autowired
+    public void setServiceServiceImpl(ServiceServiceImpl service) {
+        this.serviceService = service;
+    }
+
+    @Override
+    public void addANewSubService(SubService subService, Service service) {
+        Service services = serviceService.findServicesById(service.getId());
+        MyExceptions.isServiceAvailable(services);
+        subService.setService(service);
+        subServiceRepository.save(subService);
+    }
+
 }
