5e87c2b914784ced1b152b14d6145cf00f6deea1
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-27 12:41:50 +0100

    Our sub-services include price and descriptions. Here, we have written three methods to modify these.

--- src/main/java/com/example/finalprojectbootcamp/repositories/SubServiceRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/SubServiceRepository.java
@@ -2,6 +2,34 @@ package com.example.finalprojectbootcamp.repositories;
 
 import com.example.finalprojectbootcamp.core.entities.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface SubServiceRepository extends JpaRepository <SubService , Long> {
+public interface SubServiceRepository extends JpaRepository<SubService, Long> {
+    @Modifying
+    @Query
+            (
+                    """
+                            update SubService s set s.price=:price , s.description=:description where s.id=:id
+                            """
+            )
+    int updateSubServiceByPriceAndDescription(long id, String price, String description);
+
+    @Modifying
+    @Query
+            (
+                    """
+                            update SubService s set s.price=:price where s.id=:id
+                            """
+            )
+    int updateSubServiceByPrice(long id, String price);
+
+    @Modifying
+    @Query
+            (
+                    """
+                            update SubService s set  s.description=:description where s.id=:id
+                            """
+            )
+    int updateSubServiceByDescription(long id, String description);
 }
