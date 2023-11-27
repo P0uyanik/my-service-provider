92ccec89d7a81aa3b57672e013707ac9e0716567
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-27 12:39:41 +0100

    findServicesById takes a specific ID and returns the service associated with that ID

--- src/main/java/com/example/finalprojectbootcamp/repositories/ServiceRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/ServiceRepository.java
@@ -1,7 +1,17 @@
 package com.example.finalprojectbootcamp.repositories;
 
 import com.example.finalprojectbootcamp.core.entities.Service;
+import com.example.finalprojectbootcamp.core.entities.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ServiceRepository extends JpaRepository <Service , Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+    Service findServicesById(long id);
+
+
+
 }
