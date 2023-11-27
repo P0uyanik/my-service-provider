d6e5291bfb35b6f5dee86706024d41ac51c23f2b
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-27 12:37:01 +0100

    A method for finding services has been written within the service layer, and the service layer has implementations for navigation methods named "np" and "nn."
    The main used method here is findAll from JpaRepository, which takes Pageable as an input.

--- src/main/java/com/example/finalprojectbootcamp/services/ServiceService.java
+++ src/main/java/com/example/finalprojectbootcamp/services/ServiceService.java
@@ -2,8 +2,10 @@ package com.example.finalprojectbootcamp.services;
 
 import com.example.finalprojectbootcamp.core.entities.Customer;
 import com.example.finalprojectbootcamp.core.entities.Service;
+import org.springframework.data.domain.Page;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,4 +13,8 @@ import java.util.Optional;
 public interface ServiceService {
     /*3*/ void addANewService (Service service) ;
     /*3-1*/ Service findServicesById(long id);
+    /*5*/ List<Service> showAllServices (int pageSize) ;
+    /*5-1*/public List<Service> np() ;
+    /*5-2*/public List<Service> pp() ;
+
 }
--- src/main/java/com/example/finalprojectbootcamp/services/ServiceServiceImpl.java
+++ src/main/java/com/example/finalprojectbootcamp/services/ServiceServiceImpl.java
@@ -2,11 +2,16 @@ package com.example.finalprojectbootcamp.services;
 
 import com.example.finalprojectbootcamp.core.entities.Service;
 import com.example.finalprojectbootcamp.repositories.ServiceRepository;
+import com.example.finalprojectbootcamp.util.myExceptions.MyExceptions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
+import java.util.List;
+
 
 public class ServiceServiceImpl implements ServiceService {
-    private final ServiceRepository serviceRepository ;
+    private Page<Service> currentPage;
+    private final ServiceRepository serviceRepository;
 
     public ServiceServiceImpl(ServiceRepository serviceRepository) {
         this.serviceRepository = serviceRepository;
@@ -15,11 +20,35 @@ public class ServiceServiceImpl implements ServiceService {
 
     @Override
     public void addANewService(Service service) {
-        serviceRepository.save(service) ;
+        serviceRepository.save(service);
     }
 
     @Override
-    public Service findServicesById (long id) {
+    public Service findServicesById(long id) {
         return serviceRepository.findServicesById(id);
+    }
+
+
+    private Page<Service> findServicesWithPageSize(int pageSize) {
+        return serviceRepository.findAll(Pageable.ofSize(pageSize));
+    }
+    @Override
+    public List<Service> pp() {
+        return serviceRepository.findAll(currentPage.previousPageable()).getContent();
+    }
+    @Override
+    public List<Service> np() {
+        return serviceRepository.findAll(currentPage.nextPageable()).getContent();
+    }
+
+    @Override
+    public List<Service> showAllServices(int pageSize) {
+        MyExceptions.pageSizeIsNotCorrect(pageSize);
+
+        if (serviceRepository.count()  < pageSize)
+            pageSize = (int) serviceRepository.count() ;
+
+        this.currentPage = findServicesWithPageSize(pageSize);
+        return currentPage.getContent();
     }
 }
