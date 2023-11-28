a740a112cafe6b7c0dc0e585e1c1806b3792ab5d
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-28 11:50:40 +0100

    A method for saving an order has been written.

--- src/main/java/com/example/finalprojectbootcamp/services/OrderService.java
+++ src/main/java/com/example/finalprojectbootcamp/services/OrderService.java
@@ -1,7 +1,10 @@
 package com.example.finalprojectbootcamp.services;
 
+
+import com.example.finalprojectbootcamp.core.entities.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface OrderService {
+    void addANewOrder (Order order) ;
 }
--- src/main/java/com/example/finalprojectbootcamp/services/OrderServiceImpl.java
+++ src/main/java/com/example/finalprojectbootcamp/services/OrderServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.finalprojectbootcamp.services;
 
-public class OrderServiceImpl {
+import com.example.finalprojectbootcamp.core.entities.Order;
+import com.example.finalprojectbootcamp.repositories.OrderRepository;
+
+public class OrderServiceImpl implements  OrderService {
+    private final OrderRepository orderRepository ;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public void addANewOrder(Order order) {
+        orderRepository.save(order) ;
+    }
 }
