9d08653ea64e868cb70dce65fcb5b42de8b5f79d
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-11-28 11:41:17 +0100

    A method for finding a customer based on their ID has been written.

--- src/main/java/com/example/finalprojectbootcamp/repositories/CustomerRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/CustomerRepository.java
@@ -13,4 +13,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
                     """
     )
     int updateCustomerByPassword(long id, String password);
+    Customer findCustomerById(long id) ;
 }
