fd578d3c57f641412595e0e4c0f4274f6603d1e7
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-12-07 11:45:35 +0100

    public was added for accessibility.

--- src/main/java/com/example/finalprojectbootcamp/mapper/AdminMapper.java
+++ src/main/java/com/example/finalprojectbootcamp/mapper/AdminMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.factory.Mappers;
 import java.util.List;
 
 @Mapper
-interface AdminMapper {
+public interface AdminMapper {
     AdminMapper INSTANCE = Mappers.getMapper(AdminMapper.class) ;
     AdminDto toAdminDto (Admin admin) ;
     Admin toAdmin (AdminDto adminDto) ;
