a8d081c036e21887c489dba84dfb98486b71c581
Pouya Nikzadaghdas <puyaniek@gmail.com>
2023-12-09 15:03:53 +0100

    A query has been written for better readability in findAdminByEmailAndPassword.

--- src/main/java/com/example/finalprojectbootcamp/repositories/AdminRepository.java
+++ src/main/java/com/example/finalprojectbootcamp/repositories/AdminRepository.java
@@ -2,7 +2,13 @@ package com.example.finalprojectbootcamp.repositories;
 
 import com.example.finalprojectbootcamp.core.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AdminRepository extends JpaRepository <Admin , Long > {
-    Admin findAdminByEmailAndAndPassword(String email , String password) ;
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    @Query(
+            """
+                    select a from Admin a where a.email=:email and a.password=:password
+                    """
+    )
+    Admin findAdminByEmailAndAndPassword(String email, String password);
 }
